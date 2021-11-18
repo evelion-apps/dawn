4d65937c12e71a47d587edc1cdab32e95c8458b6
Chris Laskey <contact@chrislaskey.com>
2021-11-17 19:31:46 -0500

    Add recharts

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
 import Head from 'next/head'
+import { CartesianGrid, LineChart, Line, XAxis, YAxis } from 'recharts'
+
+const data = [
+  {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+  {name: 'Page B', uv: 500, pv: 3200, amt: 2200},
+  {name: 'Page C', uv: 600, pv: 1200, amt: 1200},
+]
 
 export default function Home() {
   return (
@@ -15,6 +22,13 @@ export default function Home() {
         <h1 className="text-6xl text-yellow-400 tracking-tighter">
           hello world
         </h1>
+
+        <LineChart isAnimationActive={false} width={400} height={200} data={data}>
+          <Line isAnimationActive={false} type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+        </LineChart>
       </main>
 
       <footer className="flex items-center justify-between w-full bg-gray-900">
